976ce27c560fa1d95e61510495b8883ee95746f8
RReDDpy <grandeandre23@gmail.com>
2025-04-28 12:53:44 +0100

    whole project xd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyD6IvqXK5hkDlpNH0hKyPXqIUToHTNfplY",
+      authDomain: "prueba-e6a3b.firebaseapp.com",
+      projectId: "prueba-e6a3b",
+      storageBucket: "prueba-e6a3b.firebasestorage.app",
+      messagingSenderId: "1006969518231",
+      appId: "1:1006969518231:web:95247384b6ac63cf3391d2"
+    })),
+    provideFirestore(() => getFirestore()),
+  ]
 };
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {SearchComponent} from './components/search/search.component';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
